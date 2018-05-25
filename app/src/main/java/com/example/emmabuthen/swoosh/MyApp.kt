3262918bb23c24545f176aae0239bfc385556f9a
EMMANY7 <nic.promise@yahoo.com>
2018-05-25 14:39:27 +0800

    skill activity

--- app/src/main/java/com/example/emmabuthen/swoosh/MyApp.kt
+++ app/src/main/java/com/example/emmabuthen/swoosh/MyApp.kt
@@ -5,39 +5,30 @@ import android.app.Application
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 
-internal class MyApp:Application() {
+class MyApp: Application() {
     override fun onCreate() {
         super.onCreate()
-        // register to be informed of activities starting up
         registerActivityLifecycleCallbacks(object: ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+                activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+
             override fun onActivityPaused(activity: Activity?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onActivityResumed(activity: Activity?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onActivityStarted(activity: Activity?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onActivityDestroyed(activity: Activity?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onActivityStarted(activity: Activity?) {
             }
 
             override fun onActivityStopped(activity: Activity?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onActivityCreated(activity: Activity,
-                                           savedInstanceState: Bundle) {
-                // new activity created; force its orientation to portrait
-                activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
             }
         })
     }
